f79bca7af77a1d1a8e4d4cb52414f93435ba3f5f
Mexaas <ruslancernaso2@gmail.com>
2026-03-28 11:05:27 +0500

    feat: added slap-timeout for server moderation. Bot content

--- Cogs/Moderation/slapCommand.py
+++ Cogs/Moderation/slapCommand.py
@@ -1,4 +1,5 @@
 import disnake
+from pathlib import Path
 from datetime import timedelta
 from disnake.ext import commands
 
@@ -6,6 +7,11 @@ class SlapCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.emojis = [1477235374127452160]
+        self.image_type = {
+            "slap_command": "Content/SlapCommand_Image.png"
+        }
+    def get_image(self, value: str) -> disnake.File:
+        return disnake.File(self.image_type[value], Path(self.image_type[value]).name)
 
     allowed_answers = ["Да", "Нет"]
     @commands.slash_command(description="Отправить пользователя подумать о своем поведении",
@@ -28,7 +34,6 @@ class SlapCommand(commands.Cog):
         ),
         время: int = commands.Param(description="Время таймаута в минутах", default=10, le=40320)
     ):
-
         if пользователь == body.author or self.bot.user == пользователь:
             return await body.response.send_message("# :x: Ошибка\n- Укажите ` правильные ` аргументы",
                 ephemeral=True, delete_after=10
@@ -36,9 +41,11 @@ class SlapCommand(commands.Cog):
         match снять_наказание.lower():
             case "да":
                 await body.response.send_message(
-                f"# {self.bot.get_emoji(self.emojis[0])} Кому-то пощадили зад!"
-                f"\n- {body.author.mention} ` решил ` оставить зад {пользователь.mention}. Он ` освобождён ` от наказания!"
-                f"\n> Причина: {причина}"
+                    f"# {self.bot.get_emoji(self.emojis[0])} Кому-то пощадили зад!"
+                    f"\n- {body.author.mention} ` решил ` оставить зад {пользователь.mention}. Он ` освобождён ` от наказания!"
+                    f"\n> Причина: {причина}",
+                    delete_after=300,
+                    file=self.get_image("slap_command")
                 )
                 return await пользователь.timeout(reason=причина, duration=None)
             case _:
@@ -51,7 +58,9 @@ class SlapCommand(commands.Cog):
                 return await body.response.send_message(
                     f"# {self.bot.get_emoji(self.emojis[0])} Кому-то дали под зад!"
                     f"\n- {body.author.mention} ` пнул ` под зад {пользователь.mention} на ` {время} ` мин."
-                    f"\n> Причина: {причина}"
+                    f"\n> Причина: {причина}",
+                    delete_after=300,
+                    file=self.get_image("slap_command")
                 )
 
 def setup(bot):
