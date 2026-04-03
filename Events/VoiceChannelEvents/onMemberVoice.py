abaa39adb735b3f71e366d9d20467f849d427d21
Mexaas <ruslancernaso2@gmail.com>
2026-04-03 17:07:07 +0500

    refactor: new logic of autovoice system

--- Events/VoiceChannelEvents/onMemberVoice.py
+++ Events/VoiceChannelEvents/onMemberVoice.py
@@ -1,4 +1,3 @@
-import disnake
 from disnake.ext import commands
 
 class CustomVoiceChannel(commands.Cog):
@@ -10,12 +9,12 @@ class CustomVoiceChannel(commands.Cog):
     async def on_voice_state_update(
             self, member, before, after
     ):
-        if (before.channel is not None and member.name.lower() in before.channel.name.lower()) and (after.channel is None or after.channel is not None):
-            await before.channel.delete()
-        category = member.guild.get_channel(self.add_channel)
-        if ((before.channel is None or before.channel is not None) and after.channel == category):
+        if before.channel and member.name.lower() in before.channel.name.lower():
+            if not before.channel.members:
+                await before.channel.delete()
+        if after.channel and after.channel.id == self.add_channel:
             new_channel = await member.guild.create_voice_channel(
-                    name=f"Комната {member.name.capitalize()}",
+                    name=f"Комната {member.display_name}",
                     category=after.channel.category
                     )
             await member.move_to(new_channel)
