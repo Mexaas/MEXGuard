1947d6cb862f1b517e87527e6ddad29dae739c79
Mexaas <ruslancernaso2@gmail.com>
2026-04-03 17:22:30 +0500

    fix: fixed error with 0 members in voice

--- Events/VoiceChannelEvents/onMemberVoice.py
+++ Events/VoiceChannelEvents/onMemberVoice.py
@@ -9,12 +9,10 @@ class CustomVoiceChannel(commands.Cog):
     async def on_voice_state_update(
             self, member, before, after
     ):
-        if not before.channel.members:
-            await before.channel.delete()
-
-        if before.channel and member.name.lower() in before.channel.name.lower():
-            if not before.channel.members:
-                await before.channel.delete()
+        if before.channel:
+            if "Комната" in before.channel.name:
+                if len(before.channel.members) == 0:
+                    await before.channel.delete()
         if after.channel and after.channel.id == self.add_channel:
             new_channel = await member.guild.create_voice_channel(
                     name=f"Комната {member.display_name}",
