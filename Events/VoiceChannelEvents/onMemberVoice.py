f14ab17ce28e63d5f00916e83cbb469f8fdce3e3
Mexaas <ruslancernaso2@gmail.com>
2026-04-03 17:37:47 +0500

    feat: added overwrites in autovoice system, creator can change and setup own channel

--- Events/VoiceChannelEvents/onMemberVoice.py
+++ Events/VoiceChannelEvents/onMemberVoice.py
@@ -14,9 +14,19 @@ class CustomVoiceChannel(commands.Cog):
                 if len(before.channel.members) == 0:
                     await before.channel.delete()
         if after.channel and after.channel.id == self.add_channel:
+            overwrites = {
+                member.guild.default_role: disnake.PermissionOverwrite(connect=True),
+                member: disnake.PermissionOverwrite
+                    manage_channels=True,
+                    move_members=True,
+                    mute_members=True,
+                    priority_speaker=True
+                )
+            }
             new_channel = await member.guild.create_voice_channel(
                     name=f"Комната {member.display_name}",
-                    category=after.channel.category
+                    category=after.channel.category,
+                    overwrites=overwrites
                     )
             await member.move_to(new_channel)
 
