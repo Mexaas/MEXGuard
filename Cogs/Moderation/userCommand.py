8745d253917b2eef52733d6e7a2c53ca687c0490
Mexaas <ruslancernaso2@gmail.com>
2026-04-04 13:58:46 +0500

    feat: added user command for administrators

---
+++ Cogs/Moderation/userCommand.py
@@ -0,0 +1,41 @@
+import disnake
+from disnake.ext import commands
+import asyncio
+from Database import database
+
+class UserFunction(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.emoji = 1477235042475315240
+
+    @commands.slash_command(description="Анализирует пользователя",
+                            guild_ids=[1466509350100013226],
+                            default_member_permissions=disnake.Permissions(administrator=True)
+    )
+    async def user(self, body: disnake.ApplicationCommandInteraction, пользователь: disnake.Member = commands.Param(
+        description="Пользователь, которого нужно проанализировать"
+    )):
+        emoji = self.bot.get_emoji(self.emoji)
+        async with database.db.execute(
+            """
+            SELECT warns_value FROM users WHERE user_id = ?
+            """,
+            (пользователь.id,)
+        ) as cursor:
+            row = await cursor.fetchone()
+            warns_value = row[0] if row else 0
+        await body.response.send_message(
+            f"Анализирую пользователя {пользователь.mention}...",
+            ephemeral=True
+        )
+        await asyncio.sleep(3)
+        await body.response.edit_message(
+            f"# {emoji} Информация об {пользователь.mention}"
+            f"\n- Предупреждения: ` {warns_value} / 3 `"
+            f"\n> Находится в войсе: ` {"Да" if пользователь.voice else "Нет"} `"
+            f"\n> Зашёл на сервер: ` {пользователь.joined_at} `"
+            f"\n> Высшая роль: {пользователь.top_role.mention}"
+        )
+
+def setup(bot):
+    bot.add_cog(UserFunction(bot))
