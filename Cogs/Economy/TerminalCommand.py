129349e02565a56c935d9808c8b8b9c21d4f7db1
Mexaas <ruslancernaso2@gmail.com>
2026-04-07 10:11:38 +0500

    feat: added new buttons and navigation

--- Cogs/Economy/TerminalCommand.py
+++ Cogs/Economy/TerminalCommand.py
@@ -4,14 +4,17 @@ from disnake.ext import commands
 from disnake.ui import View
 
 class Terminal_Back_Button(disnake.ui.Button):
-    def __init__(self, terminal):
+    def __init__(self, terminal, game_emoji: dict):
         super().__init__(
                 label="Домой",
+                emoji=disnake.PartialEmoji(name="home", id=game_emoji["Terminal_Process_Open"]),
                 style=disnake.ButtonStyle.secondary,
                 row=1
                 )
         self.terminal = terminal
+        self.game_emoji = game_emoji
     async def callback(self, body: disnake.MessageInteraction):
+        await body.response.edit_message(content=f"# <a:44503lockkey:{self.game_emoji['Terminal_Process_Open']}> Открываем...", view=None)
         await self.terminal(body)
 
 class Terminal_RoleSelect(disnake.ui.View):
@@ -66,7 +69,7 @@ class Terminal_RoleSelect(disnake.ui.View):
 
     async def on_callback_red(self, body: disnake.MessageInteraction):
         view = View()
-        view.add_item(Terminal_Back_Button(self.terminal))
+        view.add_item(Terminal_Back_Button(self.terminal, self.game_emoji))
         await body.response.edit_message(
             "Вы вступили в ` Red Team `!",
             view=view
@@ -74,7 +77,7 @@ class Terminal_RoleSelect(disnake.ui.View):
 
     async def on_callback_blue(self, body: disnake.MessageInteraction):
         view = View()
-        view.add_item(Terminal_Back_Button(self.terminal))
+        view.add_item(Terminal_Back_Button(self.terminal, self.game_emoji))
         await body.response.edit_message(
             "Вы вступили в ` Blue Team `!",
             view=view
@@ -82,7 +85,7 @@ class Terminal_RoleSelect(disnake.ui.View):
 
     async def on_callback_purple(self, body: disnake.MessageInteraction):
         view = View()
-        view.add_item(Terminal_Back_Button(self.terminal))
+        view.add_item(Terminal_Back_Button(self.terminal, self.game_emoji))
         await body.response.edit_message(
             "Вы вступили в ` Purple Team `!",
             view=view
@@ -115,9 +118,9 @@ class TerminalUI(disnake.ui.View):
 
     async def on_callback_role_select(self, body: disnake.MessageInteraction):
         view: Terminal_RoleSelect = Terminal_RoleSelect(self.game_emoji, self.terminal)
-        view.add_item(Terminal_Back_Button(self.terminal))
+        view.add_item(Terminal_Back_Button(self.terminal, self.game_emoji))
         await body.response.edit_message(
-                f"# {await body.guild.fetch_emoji(self.game_emoji['Terminal_Main_Window'])} Выбор роли\n"
+                f"# <a:97794computerlove:{self.game_emoji['Terminal_Main_Window']}> Выбор роли\n"
                 "- ` Красные ` - хакеры, занимаются нахождением уязвимостей и взломами\n"
                 "> ` Синие ` - защитники, их задача - предотвратить утечки и возможные уязвимости\n"
                 "> ` Фиолетовые ` - золотая середина между красными и синими",
@@ -134,26 +137,27 @@ class StartTerminal(commands.Cog):
             "Terminal_UI_Role_Select": 1490239879894925503,
             "BlueTeam": 1490240057922289805,
             "RedTeam": 1490240083578716211,
-            "PurpleTeam": 1490240062171123822
+            "PurpleTeam": 1490240062171123822,
+            "Star": 1477235374127452160
             }
 
     @commands.slash_command(description="Открывает терминал", guild_ids=[1466509350100013226])
     async def terminal(self, body: disnake.ApplicationCommandInteraction):
         view: TerminalUI = TerminalUI(self.game_emoji, self.terminal)
-        await body.response.send_message(
-                f"# {self.bot.get_emoji(self.game_emoji['Terminal_Process_Open'])} Открываем...",
-                ephemeral=True
-                )
-        await asyncio.sleep(2)
+        if not body.response.is_done():
+            await body.response.defer(ephemeral=True)
         await body.edit_original_response(
-                f"# {self.bot.get_emoji(self.game_emoji['Terminal_Main_Window'])} Привет, {body.author.mention}!\n"
+                f"# <a:lock:{self.game_emoji['Terminal_Process_Open']}> Открываем...",
+                )
+        await asyncio.sleep(1.2)
+        await body.edit_original_response(
+                f"# <a:terminal:{self.game_emoji['Terminal_Main_Window']}> Привет, {body.author.mention}!\n"
                 f"- Терминал: ` открыт ` (/home/**{body.author.name}**)\n"
                 "- Сторона: ` RedTeam `\n"
                 "> Управляйте терминалом через ` GUI `",
                 allowed_mentions=disnake.AllowedMentions.none(),
                 view=view
                 )
-        view.message = await body.original_response()
 
 def setup(bot):
     bot.add_cog(StartTerminal(bot))
